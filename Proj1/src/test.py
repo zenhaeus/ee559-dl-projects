431e6ff865ee7f2a94fd0323c5cf4cc9c02b3d73
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2019-03-27 10:51:46 +0100

    Add main method skeleton

---
+++ Proj1/src/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def main():
+    # Initialize data
+
+    # Initialize models
+
+    # Train models
+
+    # Generate performance metric reports
+    pass
+
+if __name__ == '__main__':
+    main()
