1b74cd631b46b90f64af93f7aaaf397c589982ac
Mauro Pfister <maurofabio@bluewin.ch>
2019-04-02 00:57:54 +0200

    Bugfix number 1 of 10000

--- Proj1/src/model.py
+++ Proj1/src/model.py
@@ -38,22 +38,23 @@ class Model(torch.nn.Module):
                 sum_loss = sum_loss + loss.item()
                 for p in self.parameters():
                     p.data.sub_(eta * p.grad.data)
-            print(e, sum_loss)
+            print(e + 1, sum_loss)
+
 
     def train_error(self):
-        return compute_nb_errors(self.train_input, self.train_input)
+        return self.compute_nb_errors(self.train_input, self.train_target)
 
     def test_error(self):
-        return compute_nb_errors(self.test_input, self.test_target)
+        return self.compute_nb_errors(self.test_input, self.test_target)
 
     def compute_nb_errors(self, input, target):
         nb_errors = 0
 
         for b in range(0, input.size(0), self.mini_batch_size):
             output = self(input.narrow(0, b, self.mini_batch_size))
-            _, predicted_classes = output.data.max(1)
+            _, predicted_targets = torch.max(output, 1)
             for k in range(self.mini_batch_size):
-                if target.data[b + k, predicted_classes[k]] <= 0:
+                if predicted_targets[k] != target[b + k]:
                     nb_errors = nb_errors + 1
 
         return nb_errors
