330de6fd357e9e328d88e478cac652af063bc641
Mauro Pfister <maurofabio@bluewin.ch>
2019-05-15 13:16:18 +0200

    Started implementing momentum for SGD

--- Proj2/mytorch/optim.py
+++ Proj2/mytorch/optim.py
@@ -15,13 +15,20 @@ class Optimizer:
 
 class SGD(Optimizer):
     """Stochastic Gradient Descent Optimizer class."""
-
+    """Math for momentum see https://towardsdatascience.com/stochastic-gradient-descent-with-momentum-a84097641a5d """
     def __init__(self, params, lr=0.01, momentum=0):
         super(SGD, self).__init__(params)
         self.lr = lr
         self.momentum = momentum
+        if (self.momentum != 0):
+            self.Vt_prev_buffer = 0 # TODO find a good way to store previous gradients of each parameter (dictionary?)
 
     def step(self):
         """Perform a single optimization step"""
-        for param in self.params:
-            param[0].add_(-self.lr*param[1])
+        for p, p_grad in self.params:
+            Vt = p_grad
+            if (self.momentum != 0):
+                Vt = self.momentum*Vt_prev_buffer + (1-self.momentum)*Vt
+
+            # update parameter
+            p.add_(-self.lr*Vt)
