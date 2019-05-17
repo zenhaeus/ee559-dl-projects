eee129eac3425034fe568a79979620aa426e73fd
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2019-05-17 16:23:22 +0200

    Add report makefiles
    update create submission script

--- create-submission.sh
+++ create-submission.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
-zip -r Proj_285467_235440_226652.zip Proj1 Proj2
+# Compile report 1
+cd ./Proj1/report
+make
+cd ../../
+
+# Compile report 1
+cd ./Proj1/report
+make
+cd ../../
+
+zip -r Proj_285467_235440_226652.zip Proj1 Proj2 \
+    --exclude Proj1/data\* \
+    **/__pycache__\* \
+    **/.ipynb_checkpoints\* \
+    \*.aux \
+    \*.blg \
+    \*.ipynb \
+    \*.out \
+    \*.bib \
+    \*.cls \
+    \*.sty \
+    \*.bst \
+    \*.log \
+    \*.bbl \
+    **/report/Makefile \
+    **/report/.gitignore \
+    Proj1/output\*
